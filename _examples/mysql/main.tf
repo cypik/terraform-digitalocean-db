5c98cbf9bd8dd8d90874f571b7ed06ed69955c17
chandrahaskoneru <123540688+chandrahaskoneru@users.noreply.github.com>
2023-11-24 14:09:14 +0530

    Update main.tf

--- _examples/mysql/main.tf
+++ _examples/mysql/main.tf
@@ -4,7 +4,7 @@ provider "digitalocean" {}
 
 
 module "vpc" {
-  source      = "git::https://github.com/opz0/terraform-digitalocean-vpc.git?ref=v1.0.0"
+  source      = "git::https://github.com/cypik/terraform-digitalocean-vpc.git?ref=v1.0.0"
   name        = "app"
   environment = "test"
   region      = "blr1"
